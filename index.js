cfdacf88318bf2212f5667fa72f850647c6cef89
devtedd <devtedd@gmail.com>
2015-11-05 15:53:20 +0000

    working on a workable application skeleton

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-var Hapi = require('hapi')
+var server = require('./lib/server')
 
-var server = new Hapi.Server()
-var port = process.env.PORT || 8080
 
-server.connection({
-  host: '0.0.0.0',
-  port: port
-})
-
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: function (request, reply) {
-    reply('hello world')
-  }
-})
 
 server.start(
-  function (){
+  function () {
     console.log('Hapi server running at: ', server.info.uri)
   }
 )
