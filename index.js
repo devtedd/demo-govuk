abc7ba88a372d116f1ac32c809c2c16969e5f017
devtedd <devtedd@gmail.com>
2016-01-05 15:34:22 +0000

    work for console logging

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var server = require('./lib/server')
 
 server.start(
-  function () {
-    console.log('Hapi server running at: ', server.info.uri)
+  function (err) {
+    if(err){
+      server.log('error', err)
+    }
+    server.log('info', 'Hapi server running at: ' + server.info.uri)
   }
 )
